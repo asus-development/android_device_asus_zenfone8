f34bc557d67bfde0d6bfb01b38d316a70c9f13c7
Alexander Koskovich <zvnexus@outlook.com>
2022-01-26 13:56:16 +0100

    zenfone8: Move all HIDL XMLs to "hidl" subdirectory.
     * There are enough of these now that a new directory
       is justified. Ideally root of tree shouldn't have
       too many individual files.
    
    Change-Id: I1d5123dc929bb93c0d6176126cce08ad808b542b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -119,11 +119,11 @@ TARGET_USERIMAGES_SPARSE_EXT_DISABLED := false
 BOARD_EXT4_SHARE_DUP_BLOCKS := true
 
 #HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/vendor_framework_compatibility_matrix.xml
-DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/framework_manifest.xml
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest_lahaina.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/hidl/vendor_framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/hidl/framework_manifest.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/hidl/manifest_lahaina.xml
 ODM_MANIFEST_SKUS := eSE
-ODM_MANIFEST_ESE_FILES := $(DEVICE_PATH)/eSE_manifest.xml
+ODM_MANIFEST_ESE_FILES := $(DEVICE_PATH)/hidl/eSE_manifest.xml
 
 # Kernel
 KERNEL_LD := LD=ld.lld
