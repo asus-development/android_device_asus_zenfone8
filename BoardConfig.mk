7725fcc916f7f2aa271af852ef5ca1804fbd941a
Alexander Koskovich <zvnexus@outlook.com>
2022-01-26 13:54:32 +0100

    zenfone8: Only support eSE for the Japan variant.
    Change-Id: Ie2891c5ea186c9e19e7cda7e69975e0471ca77ec

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -122,6 +122,8 @@ BOARD_EXT4_SHARE_DUP_BLOCKS := true
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/vendor_framework_compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest_lahaina.xml
+ODM_MANIFEST_SKUS := eSE
+ODM_MANIFEST_ESE_FILES := $(DEVICE_PATH)/eSE_manifest.xml
 
 # Kernel
 KERNEL_LD := LD=ld.lld
